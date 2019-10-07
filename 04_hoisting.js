509bfff9876ce4fb0a2e5a5d41ec465556dd0ce8
AABorisov <borisov.andrew.a@gmail.com>
2019-10-08 00:01:38 +0300

    hoisting

--- 04_hoisting.js
+++ 04_hoisting.js
@@ -1,3 +1,35 @@
+console.log(sum(1, 41))
+
 function sum(a, b) {
     return a + b
 }
+
+console.log(i)
+var i = 42
+console.log(i)
+
+try {
+
+    console.log(num)
+} catch (e) {
+    console.log(e)
+}
+const num = 42
+console.log(num)
+
+// function expression & Function declaration
+try {
+
+    console.log(square(25))
+} catch (e) {
+    console.log(e)
+}
+
+// function square(num) {
+//     return num ** 2
+// }
+var square = function(num) {
+    return num ** 2
+}
+
+console.log(square(25))
