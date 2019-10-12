5e7f68118ae0eb03e6975f66f166a18dab30fb16
AABorisov <borisov.andrew.a@gmail.com>
2019-10-12 23:59:10 +0300

    Patterns

---
+++ patterns/03_behaviour/8_template.js
@@ -0,0 +1,44 @@
+class Employee {
+    constructor(name, salary) {
+        this.name = name
+        this.salary = salary
+    }
+
+    responsibilities() {}
+
+    work() {
+        return `${this.name} does ${this.responsibilities}`
+    }
+
+    getPaid() {
+        return `${this.name} has salary ${this.salary}`
+    }
+}
+
+class Developer extends Employee {
+    constructor(name, salary) {
+        super(name, salary)
+    }
+
+    responsibilities() {
+        return 'developing'
+    }
+}
+
+class Tester extends Employee {
+    constructor(name, salary) {
+        super(name, salary)
+    }
+
+    responsibilities() {
+        return 'testing'
+    }
+}
+
+const dev = new Developer('Andrey', 100000)
+console.log(dev.getPaid())
+console.log(dev.work())
+
+const tester = new Tester('Elena', 90000)
+console.log(tester.getPaid())
+console.log(tester.work())
